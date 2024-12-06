ddab5d2ced777e30eb123ff0977cc8760ec101a2
Woong Jung <kaka366@naver.com>
2024-12-06 10:11:01 +0900

    FEAT : 지출추가하기에 카테고리없음

--- src/pages/Floating/ManualReceipt.js
+++ src/pages/Floating/ManualReceipt.js
@@ -1,6 +1,6 @@
 // src/pages/Floating/ManualReceipt.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Platform, StatusBar } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Platform, StatusBar, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import DatePicker from '../../components/FloatingTab/DatePicker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -19,7 +19,7 @@ const ManualReceipt = ({ navigation, route }) => {
   const [selectedCategory, setSelectedCategory] = useState(null);
 
 
-  
+
   const moods = ['🤩', '😊', '😑', '🥲', '😭'];
 
   const categories = [
@@ -56,9 +56,9 @@ const ManualReceipt = ({ navigation, route }) => {
       const receipts = existingData ? JSON.parse(existingData) : [];
       receipts.push(newReceipt);
       await AsyncStorage.setItem('receipts', JSON.stringify(receipts));
-      
+
       alert('영수증이 저장되었습니다.');
-      
+
       const previousScreen = route.params?.previousScreen || 'AccountBook';
       navigation.navigate(previousScreen);
     } catch (error) {
@@ -69,115 +69,120 @@ const ManualReceipt = ({ navigation, route }) => {
 
   return (
     <View style={styles.container}>
-      {/* 입력 필드들 */}
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>사업자 번호</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="사업자 번호 10자리를 입력해 주세요"
-          value={businessNumber}
-          onChangeText={setBusinessNumber}
-          keyboardType="numeric"
-          placeholderTextColor="#949494"
-        />
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        {/* 입력 필드들 */}
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>사업자 번호</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="사업자 번호 10자리를 입력해 주세요"
+            value={businessNumber}
+            onChangeText={setBusinessNumber}
+            keyboardType="numeric"
+            placeholderTextColor="#949494"
+          />
 
-        <Text style={styles.label}>대표자</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="대표자를 입력해 주세요"
-          value={representative}
-          onChangeText={setRepresentative}
-        />
+          <Text style={styles.label}>대표자</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="대표자를 입력해 주세요"
+            value={representative}
+            onChangeText={setRepresentative}
+          />
 
-        <Text style={styles.label}>상호</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="상호를 입력해 주세요"
-          value={storeName}
-          onChangeText={setStoreName}
-        />
+          <Text style={styles.label}>상호</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="상호를 입력해 주세요"
+            value={storeName}
+            onChangeText={setStoreName}
+          />
 
-        <Text style={styles.label}>금액</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="금액을 입력해 주세요"
-          value={amount}
-          onChangeText={setAmount}
-          keyboardType="numeric"
-        />
+          <Text style={styles.label}>금액</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="금액을 입력해 주세요"
+            value={amount}
+            onChangeText={setAmount}
+            keyboardType="numeric"
+          />
 
-        <Text style={styles.label}>발행일</Text>
-        <TouchableOpacity 
-          activeOpacity={0.7}
-          onPress={() => setDatePickerVisible(true)}
-        >
-          <View style={styles.input}>
-            <Text style={[styles.dateText]}>
-              {formatDate(selectedDate)}
-            </Text>
+          <Text style={styles.label}>발행일</Text>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => setDatePickerVisible(true)}
+          >
+            <View style={styles.input}>
+              <Text style={[styles.dateText]}>
+                {formatDate(selectedDate)}
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          <DatePicker
+            isVisible={isDatePickerVisible}
+            onClose={() => setDatePickerVisible(false)}
+            onSelect={setSelectedDate}
+            selectedDate={selectedDate}
+          />
+
+          <Text style={styles.label}>오늘 기분 어떠세요?</Text>
+          <View style={styles.moodContainer}>
+            {moods.map((mood, index) => (
+              <TouchableOpacity
+                key={index}
+                style={[
+                  styles.moodButton,
+                  selectedMood === index && styles.selectedMood
+                ]}
+                onPress={() => setSelectedMood(index)}
+              >
+                <Text style={styles.moodEmoji}>{mood}</Text>
+              </TouchableOpacity>
+            ))}
           </View>
-        </TouchableOpacity>
-        
-        <DatePicker
-          isVisible={isDatePickerVisible}
-          onClose={() => setDatePickerVisible(false)}
-          onSelect={setSelectedDate}
-          selectedDate={selectedDate}
-        />
 
-        <Text style={styles.label}>오늘 기분 어떠세요?</Text>
-        <View style={styles.moodContainer}>
-          {moods.map((mood, index) => (
-            <TouchableOpacity
-              key={index}
-              style={[
-                styles.moodButton,
-                selectedMood === index && styles.selectedMood
-              ]}
-              onPress={() => setSelectedMood(index)}
-            >
-              <Text style={styles.moodEmoji}>{mood}</Text>
-            </TouchableOpacity>
-          ))}
+          {/* 카테고리 선택 */}
+          {/* <View style={styles.categorySection}>
+            <Text style={styles.label}>카테고리</Text>
+            <View style={styles.categoryContainer}>
+              {categories.map((category) => (
+                <TouchableOpacity
+                  key={category.id}
+                  style={[
+                    styles.categoryItem,
+                    selectedCategory === category.id && styles.selectedCategory,
+                  ]}
+                  onPress={() => setSelectedCategory(category.id)}
+                >
+                  <Text style={styles.categoryEmoji}>{category.emoji}</Text>
+                  <Text style={styles.categoryName}>{category.name}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </View> */}
+          <View style={styles.categorySection}>
+            <Text style={styles.label}>카테고리</Text>
+            <View style={styles.horizontalCategoryContainer}>
+              {categories.map((category) => (
+                <TouchableOpacity
+                  key={category.id}
+                  style={[
+                    styles.categoryItem,
+                    selectedCategory === category.id && styles.selectedCategory,
+                  ]}
+                  onPress={() => setSelectedCategory(category.id)}
+                >
+                  <Text style={styles.categoryEmoji}>{category.emoji}</Text>
+                  <Text style={styles.categoryName}>{category.name}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </View>
         </View>
-
-
-      {/* 카테고리 선택 */}
-      <View style={styles.categorySection}>
-        <Text style={styles.label}>카테고리</Text>
-        <View style={styles.categoryContainer}>
-          {categories.map((category) => (
-            <TouchableOpacity
-              key={category.id}
-              style={[
-                styles.categoryItem,
-                selectedCategory === category.id && styles.selectedCategory,
-              ]}
-              onPress={() => setSelectedCategory(category.id)}
-            >
-              <Text style={styles.categoryEmoji}>{category.emoji}</Text>
-              <Text style={styles.categoryName}>{category.name}</Text>
-            </TouchableOpacity>
-          ))}
-        </View>
-      </View>
-
-
-
-
-
-
-      </View>
-
-
-
-
-
-
-
-
-
-
+         {/* 공백 추가 */}
+    <View style={styles.bottomSpacing} />
+      </ScrollView>
 
       {/* 저장 버튼 */}
       <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
@@ -188,6 +193,89 @@ const ManualReceipt = ({ navigation, route }) => {
 };
 
 const styles = StyleSheet.create({
+  // categorySection: {
+  //   marginTop: 16,
+  //   marginBottom: 16,
+  // },
+  // horizontalCategoryContainer: {
+  //   flexDirection: 'row',
+  //   flexWrap: 'wrap', // 필요한 경우 다음 줄로 넘기기
+  //   gap: 8, // 항목 간의 간격
+  // },
+  // categoryItem: {
+  //   width: 70,
+  //   height: 70,
+  //   backgroundColor: '#F2F2F2',
+  //   borderRadius: 6,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   marginHorizontal: 8, // 양옆 여백
+  //   marginBottom: 10, // 세로 여백
+  // },
+  // selectedCategory: {
+  //   backgroundColor: '#73E0D6',
+  //   borderWidth: 1,
+  //   borderColor: 'rgba(0, 0, 0, 0.10)',
+  // },
+  // categoryEmoji: {
+  //   fontSize: 26,
+  //   color: '#6C6C6C',
+  //   fontFamily: 'Pretendard',
+  //   fontWeight: '500',
+  // },
+  // categoryName: {
+  //   fontSize: 12,
+  //   color: '#6C6C6C',
+  //   fontFamily: 'Pretendard',
+  //   fontWeight: '400',
+  //   marginTop: 6, // 이모지와 텍스트 간의 간격
+  //   textAlign: 'center',
+  // },
+  scrollViewContent: {
+    flexGrow: 1,
+    paddingBottom: 50, // 여유 공간 추가
+  },
+  bottomSpacing: {
+    height: 150, // "저장하기" 버튼과 카테고리 사이 공백 추가
+  },
+  categorySection: {
+    marginTop: 16,
+    marginBottom: 16,
+  },
+  horizontalCategoryContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8, // 아이템 간 간격
+  },
+  categoryItem: {
+    width: 60, // 크기 조정
+    height: 60,
+    backgroundColor: '#F2F2F2',
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 8, // 아이템 간 간격 조정
+  },
+  selectedCategory: {
+    backgroundColor: '#73E0D6',
+    borderWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.10)',
+  },
+  categoryEmoji: {
+    fontSize: 24,
+    color: '#6C6C6C',
+    fontFamily: 'Pretendard',
+    fontWeight: '500',
+  },
+  categoryName: {
+    fontSize: 10, // 텍스트 크기 조정
+    color: '#6C6C6C',
+    fontFamily: 'Pretendard',
+    fontWeight: '400',
+    marginTop: 4,
+    textAlign: 'center',
+  },
+  
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
